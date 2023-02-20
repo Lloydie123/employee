a2410ec2478e72543b183714fedb4b52862763fc
Lloydie123 <jhonlloydborlagdan8@gmail.com>
2023-02-20 11:06:52 +0800

    aaaaaaa

--- employee/urls.py
+++ employee/urls.py
@@ -15,17 +15,6 @@ urlpatterns = [
      
      #Path for department
      
-     path('create_department/',views.MemberCreate.as_view(),name='MemberCreate'),
-     path('emp', views.emp),  
-     path('show',views.show),  
-     path('edit/<int:id>', views.edit), 
-     path('update/<int:id>', views.update),  
-     path('delete/<int:id>', views.destroy),  
-     
-     
-     path('new', views.book_create, name='book_create'),
-     path('list', views.book_list, name='book_list'),
-     path('<int:pk>/edit', views.book_edit, name='book_edit'),
-     path('<int:pk>/delete', views.book_delete, name='book_delete'),
+   
     
 ]
